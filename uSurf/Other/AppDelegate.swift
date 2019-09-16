758c3d8d51e5c5778bfae38c3f47c3270351152b
Matthew Jagiela <matthewjagiela@icloud.com>
2019-09-15 22:38:47 -0400

    iPhone Auto-Rotation for settings fixed + added new class for rotations

--- uSurf/Other/AppDelegate.swift
+++ uSurf/Other/AppDelegate.swift
@@ -8,9 +8,10 @@
 
 import UIKit
 import GoogleMobileAds
-//swiftlint:disable force_cast
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var orientationLock = UIInterfaceOrientationMask.all
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
@@ -20,28 +21,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
     func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
-        if let rootViewController = self.topViewControllerWithRootViewController(rootViewController: window?.rootViewController) {
-            if rootViewController.responds(to: #selector(TabViewController.canRotate)) {
-                // Unlock landscape view orientations for this view controller
-                return .allButUpsideDown
-            }
-        }
-        // Only allow portrait (standard behaviour)
-        return .portrait
+        return orientationLock
     }
     
-    private func topViewControllerWithRootViewController(rootViewController: UIViewController!) -> UIViewController? {
-        if rootViewController == nil { return nil }
-        if rootViewController.isKind(of: UITabBarController.self) {
-            return topViewControllerWithRootViewController(rootViewController: (rootViewController as! UITabBarController).selectedViewController)
-        } else if rootViewController.isKind(of: UINavigationController.self) {
-            return topViewControllerWithRootViewController(rootViewController: (rootViewController as! UINavigationController).visibleViewController)
-        } else if rootViewController.presentedViewController != nil {
-            return topViewControllerWithRootViewController(rootViewController: rootViewController.presentedViewController)
-        }
-        return rootViewController
-    }
-
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
