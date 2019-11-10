0d4b09783eb646c4d613759730b0496c5715aeaa
matthewjagiela <matthewjagiela@icloud.com>
2019-11-09 19:30:50 -0500

    Codables for info (#61)
    * Class for the decodables.
    
    * Initializer is set.
    
    * Variable changes, update to internet label function to use JSON

--- uSurf/Handelers/VersionHandler.swift
+++ uSurf/Handelers/VersionHandler.swift
@@ -9,6 +9,24 @@
 import UIKit
 
 class VersionHandler: NSObject {
+    var internetInfo: InternetInformation?
+    override init() {
+        super.init()
+        if let jsonURL = URL(string: "https://raw.githubusercontent.com/matthewjagiela/uApps-JSON/master/uAppsInfo.json") {
+            URLSession.shared.dataTask(with: jsonURL) { data, _, error in
+                if let fetchedData = data {
+                    let decoder = JSONDecoder()
+                    do {
+                        self.internetInfo = try decoder.decode(InternetInformation.self, from: fetchedData)
+            
+                    } catch {
+                        print("An Error Has Occured \(error)")
+                    }
+                }
+            }.resume()
+        }
+        
+    }
     
     func getAppVersion() -> String {
         return "Currently Running: 6.1"
@@ -33,4 +51,17 @@ class VersionHandler: NSObject {
         return ""
     }
 
+}
+open class InternetInformation: NSObject, Decodable {
+    public var uSurfVersion: String?
+    public var uAppsNews: String?
+    enum CodingKeys: String, CodingKey {
+        case uTimeVersion = "uSurf_Version"
+        case uAppsNews =  "uApps_News"
+    }
+    public required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        uSurfVersion = try? container.decode(String.self, forKey: .uTimeVersion)
+        uAppsNews = try? container.decode(String.self, forKey: .uAppsNews)
+    }
 }
