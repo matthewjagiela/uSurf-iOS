973a5c8068ae8121c4f48787ea0febb787b6aa98
Matthew Jagiela <matthewjagiela@icloud.com>
2019-12-03 23:06:04 -0500

    Adding of snow to uSurf.

---
+++ uSurf/Other/SnowScene.h
@@ -0,0 +1,14 @@
+//
+//  SnowScene.h
+//  uSurf
+//
+//  Created by Matthew Jagiela on 11/22/15.
+//  Copyright © 2015 Matthew Jagiela. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+@import SpriteKit;
+
+@interface SnowScene : SKScene
+
+@end
