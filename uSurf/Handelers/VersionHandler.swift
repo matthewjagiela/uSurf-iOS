0e96ce955986afea29f428ce179de49cdf29ac29
matthewjagiela <matthewjagiela@icloud.com>
2019-12-04 19:47:26 -0500

    Info Update V 6.3 (#78)
    * Bug fix for the privacy policy
    
    * Snow falling info
    
    * Holiday icon with a small swiftlint fix.
    
    * 6.3 update

--- uSurf/Handelers/VersionHandler.swift
+++ uSurf/Handelers/VersionHandler.swift
@@ -29,7 +29,7 @@ class VersionHandler: NSObject {
     }
     
     func getAppVersion() -> String {
-        return "Currently Running: 6.2"
+        return "Currently Running: 6.3"
     }
     func getUpdateInformation() -> String {
         if let path = Bundle.main.path(forResource: "Changes", ofType: "txt") {
