d42ecbacd5b79a8dfdd08e6f5375c629e0a7c18c
Matthew Jagiela <matthewjagiela@icloud.com>
2021-12-13 21:50:02 -0500

    Side Menu Controller Host

---
+++ uSurf/ViewControllers/Universal/SideMenuHostViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SideMenuHostViewController.swift
+//  uSurf
+//
+//  Created by Matthew Jagiela on 12/13/21.
+//  Copyright © 2021 Matthew Jagiela. All rights reserved.
+//
+
+import UIKit
+
+class SideMenuHostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
