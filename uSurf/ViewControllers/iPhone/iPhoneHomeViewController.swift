2af46dc9f58a8e1c75bc70355a574acc98f684fd
Matthew Jagiela <matthewjagiela@icloud.com>
2022-05-29 16:27:34 -0700

    iPhone segue

--- uSurf/ViewControllers/iPhone/iPhoneHomeViewController.swift
+++ uSurf/ViewControllers/iPhone/iPhoneHomeViewController.swift
@@ -212,6 +212,11 @@ class iPhoneHomeViewController: UIViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        
+        if segue.identifier == "goSettings" {
+            guard let vc = segue.destination as? SettingsViewController else { return }
+            vc.homeDelegate = self
+        }
     }
 
     override var preferredStatusBarStyle: UIStatusBarStyle {
