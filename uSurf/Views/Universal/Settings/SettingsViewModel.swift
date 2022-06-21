a79549e0ab11ca3e3ec821a6c400fe59827affd1
Matthew Jagiela <matthewjagiela@icloud.com>
2022-06-20 23:23:36 -0400

    vm init
    Signed-off-by: Matthew Jagiela <matthewjagiela@icloud.com>

---
+++ uSurf/Views/Universal/Settings/SettingsViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsViewModel.swift
+//  uSurf
+//
+//  Created by Matthew Jagiela on 6/20/22.
+//  Copyright © 2022 Matthew Jagiela. All rights reserved.
+//
+
+import Foundation
+
+class SettingsViewModel {
+    
+}
