5ce2a9223078aeef43a47aa132b29857cb32d497
Matthew Jagiela <matthewjagiela@icloud.com>
2022-08-03 23:08:24 -0700

    Fetching the new tab data and decoding it
    Signed-off-by: Matthew Jagiela <matthewjagiela@icloud.com>

--- uSurf/Data/NewTabHandler.swift
+++ uSurf/Data/NewTabHandler.swift
@@ -8,14 +8,54 @@
 
 import Foundation
 
-struct Tabs: Codable, Hashable {
+enum TabErrors: Error {
+    case encodingError
+    case decodingError
+    case iCloudError
+}
+
+struct Tab: Codable, Hashable {
     var name: String
     var url: String
     var image: Data
 }
 
 class TabHandler {
+    final fileprivate var iPhoneTabIdentifier = "iPhoneNewTabs"
+    final fileprivate var iPadTabIdentifier = "iPadNewTabs"
+    var iPhoneTabs: [Tab] = []
+    var iPadTabs: [Tab] = []
+    
+    init() throws {
+        do {
+            self.iPhoneTabs = try self.getiPhoneTabs()
+            self.iPadTabs = try self.getiPadTabs()
+        } catch {
+            throw error
+        }
+    }
     
     
+    // MARK: - Setters
     
+    // MARK: - Getters
+    func getiPhoneTabs() throws -> [Tab] {
+        guard let tabData = NSUbiquitousKeyValueStore.default.data(forKey: iPhoneTabIdentifier) else { throw TabErrors.iCloudError }
+        do {
+            let decodedTabs = try JSONDecoder().decode([Tab].self, from: tabData)
+            return decodedTabs
+        } catch {
+            throw TabErrors.decodingError
+        }
+    }
+    
+    func getiPadTabs() throws -> [Tab] {
+        guard let tabData = NSUbiquitousKeyValueStore.default.data(forKey: iPadTabIdentifier) else { throw TabErrors.iCloudError }
+        do {
+            let decodedTabs = try JSONDecoder().decode([Tab].self, from: tabData)
+            return decodedTabs
+        } catch {
+            throw TabErrors.decodingError
+        }
+    }
 }
