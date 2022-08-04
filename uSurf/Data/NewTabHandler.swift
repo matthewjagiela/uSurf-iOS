f5a7aff4339a6fc42ab6853326fbf50428f2dc81
Matthew Jagiela <matthewjagiela@icloud.com>
2022-08-03 22:57:33 -0700

    Basic handler setup
    Signed-off-by: Matthew Jagiela <matthewjagiela@icloud.com>

---
+++ uSurf/Data/NewTabHandler.swift
@@ -0,0 +1,21 @@
+//
+//  NewTabHandler.swift
+//  uSurf
+//
+//  Created by Matthew Jagiela on 8/3/22.
+//  Copyright © 2022 Matthew Jagiela. All rights reserved.
+//
+
+import Foundation
+
+struct Tabs: Codable, Hashable {
+    var name: String
+    var url: String
+    var image: Data
+}
+
+class TabHandler {
+    
+    
+    
+}
