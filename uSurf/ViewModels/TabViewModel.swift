864b31095564eb74de73ae64bb5c08c908beadb2
Matthew Jagiela <matthewjagiela@icloud.com>
2022-08-07 12:39:15 -0700

    Tab files

---
+++ uSurf/ViewModels/TabViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  TabViewModel.swift
+//  uSurf
+//
+//  Created by Matthew Jagiela on 8/7/22.
+//  Copyright © 2022 Matthew Jagiela. All rights reserved.
+//
+
+import Foundation
---
+++ uSurf/Views/Universal/iPhoneTabViewController.swift
@@ -0,0 +1,30 @@
+//
+//  iPhoneTabViewController.swift
+//  uSurf
+//
+//  Created by Matthew Jagiela on 8/7/22.
+//  Copyright © 2022 Matthew Jagiela. All rights reserved.
+//
+
+import UIKit
+
+class iPhoneTabViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
